f5ea5a943c421fd682fe68b157a3374595526abe
Mitchell <37925797+ping-localhost@users.noreply.github.com>
2025-08-29 10:01:52 +0000

    Renamed everything to Panda Status

--- custom_components/panda_status/const.py
+++ custom_components/panda_status/const.py
@@ -1,8 +1,8 @@
-"""Constants for integration_blueprint."""
+"""Constants for panda_status."""
 
 from logging import Logger, getLogger
 
 LOGGER: Logger = getLogger(__package__)
 
-DOMAIN = "integration_blueprint"
+DOMAIN = "panda_status"
 ATTRIBUTION = "Data provided by http://jsonplaceholder.typicode.com/"
--- custom_components/panda_status/switch.py
+++ custom_components/panda_status/switch.py
@@ -1,4 +1,4 @@
-"""Switch platform for integration_blueprint."""
+"""Switch platform for panda_status."""
 
 from __future__ import annotations
 
@@ -17,7 +17,7 @@ if TYPE_CHECKING:
 
 ENTITY_DESCRIPTIONS = (
     SwitchEntityDescription(
-        key="integration_blueprint",
+        key="panda_status",
         name="Integration Switch",
         icon="mdi:format-quote-close",
     ),
@@ -40,7 +40,7 @@ async def async_setup_entry(
 
 
 class IntegrationBlueprintSwitch(IntegrationBlueprintEntity, SwitchEntity):
-    """integration_blueprint switch class."""
+    """panda_status switch class."""
 
     def __init__(
         self,
